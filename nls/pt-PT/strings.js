879e5d9ce7456503c6a3f2daecc397c020efc5c9
Carlos Santos <carlos.santos@innabler.co.uk>
2014-11-12 10:21:28 +0000

    Add Portuguese from Portugal translation

---
+++ nls/pt-PT/strings.js
@@ -0,0 +1,15 @@
+// Portuguese (Portugal) - root strings
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+  'COMPARE_VERTICAL': 'Comparar na Vertical',
+  'COMPARE_HORIZONTAL': 'Comparar na Horizontal',
+  'COMPARE_FILE' : 'Comparar com ficheiro...',
+  'COMPARE_HISTORY' : 'Comparar histórico',
+  'CHOOSE_FILE' : 'Escolher um ficheiro...',
+  'SAVE_FILES' : 'Guardar ficheiro(s)',
+  'CLOSE_VIEWS' : 'Sair da Comparação',
+  'TOGGLE_STICKY' : 'Afixar/Desafixar'
+});
