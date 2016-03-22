4c24696a8d221c8e058e892feb6d70cb2c10bc11
bomsy <b4bomsy@gmail.com>
2016-03-22 00:34:03 +0000

    remove generic module loading

--- src/codemirror/merge.js
+++ src/codemirror/merge.js
@@ -2,15 +2,6 @@
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
 // declare global: diff_match_patch, DIFF_INSERT, DIFF_DELETE, DIFF_EQUAL
-
-/*(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror")); // Note non-packaged dependency diff_match_patch
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror", "diff_match_patch"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {*/
 define(function (require, exports, module) {
   "use strict";
   
